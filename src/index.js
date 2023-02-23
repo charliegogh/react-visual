03985285038b196d82214a9c9f2eefd496cda451
charlie <44202644+charliegogh@users.noreply.github.com>
2023-02-23 17:04:21 +0800

    00

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-
+console.log(11);
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
