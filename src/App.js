b838dc47615c81ccbae2e28ac8dd2817db06653b
charlie <44202644+charliegogh@users.noreply.github.com>
2023-02-23 17:14:27 +0800

    ..

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Chart from "./components/echarts/chart";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import moment from "moment";
 export default function App() {
   const [options, setOptions] = useState({});
   useEffect(() => {
     // 使用异步请求
-    const test = async () => {
-      console.log(moment());
-      const rs = await axios(
-        "https://cycx.cnki.net/apinew/houtai/regionbase/list?level=2"
-      );
-    };
-    test();
+    // const test = async () => {
+    //   const rs = await axios(
+    //     "https://cycx.cnki.net/apinew/houtai/regionbase/list?level=2"
+    //   );
+    // };
+    // test();
   }, []);
   return (
     <div className="App">
+      11
       <Chart options={options} />
     </div>
   );
